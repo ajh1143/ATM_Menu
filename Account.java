7ecf230de69cbe9fad9a43125b07d8af0b05c4af
ajh1143 <15341747+ajh1143@users.noreply.github.com>
2017-07-26 17:47:30 -0700

    Update Account.java

--- Account.java
+++ Account.java
@@ -7,36 +7,40 @@ public class Account {
         private double deposit;
         private double withdraw;
 
+        //Sets the username
         void setName(String _userName)
         {
             userName = _userName;
         }
-
+        //Sets initial deposit amount
         void setDeposit(double _amount){
             balance += _amount;
         }
-
+        //Sets withdrawal amount
         void setWithdraw(double _amount){
             balance -= _amount;
         }
-
+        //Sets user's address
         void setAddress(String _address)
         {
             address = _address;
         }
+        
         void setBalance(double amount)
         {
             balance += amount;
         }
+        //Returns the customer's username
         String getName()
         {
             return userName;
         }
-
+        //Returns the customer's address
         String getAddress()
         {
             return address;
         }
+        //Returns the customer's balance
         double getBalance()
         {
             return balance;
