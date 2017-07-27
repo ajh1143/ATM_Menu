dec011e3de1c1db5b4bdfecd3cde7b4e8ca0d1a7
ajh1143 <ajh1143@users.noreply.github.com>
2017-07-26 17:07:16 -0700

    Update Account.java
    Getters and Setters for account information

--- Account.java
+++ Account.java
@@ -1,1 +1,46 @@
+package com.company;
+
+public class Account {
+        private String userName;
+        private String address;
+        private double balance;
+        private double deposit;
+        private double withdraw;
+
+        void setName(String _userName)
+        {
+            userName = _userName;
+        }
+
+        void setDeposit(double _amount){
+            balance += _amount;
+        }
+
+        void setWithdraw(double _amount){
+            balance -= _amount;
+        }
+
+        void setAddress(String _address)
+        {
+            address = _address;
+        }
+        void setBalance(double amount)
+        {
+            balance += amount;
+        }
+        String getName()
+        {
+            return userName;
+        }
+
+        String getAddress()
+        {
+            return address;
+        }
+        double getBalance()
+        {
+            return balance;
+        }
+
+}
 
